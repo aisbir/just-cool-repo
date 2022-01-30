fd18f60caacfbc85c615529955865b6793e6e383
aisbir Gans <75553444+aisbir@users.noreply.github.com>
2022-01-30 19:43:38 +0700

    Add files via upload

---
+++ anu.js
@@ -0,0 +1,63 @@
+function ngopisiji () {
+    document.getElementById("cmdsiji").select();
+     document.execCommand("copy");                 
+     Swal.fire({
+      icon: 'success',
+      title: 'Copyed',
+      text: 'Your work has been saved on your clipboard :3',
+    })
+    };
+    function ngopiloro () {
+    document.getElementById("cmdloro").select();
+     document.execCommand("copy");                 
+     Swal.fire({
+      icon: 'success',
+      title: 'Copyed',
+      text: 'Your work has been saved on your clipboard :3',
+    })
+    };
+    function ngopitelu () {
+    document.getElementById("cmdtelu").select();
+     document.execCommand("copy");                 
+     Swal.fire({
+      icon: 'success',
+      title: 'Copyed',
+      text: 'Your work has been saved on your clipboard :3',
+    })
+    };
+    function ngopipapat () {
+    document.getElementById("cmdpapat").select();
+     document.execCommand("copy");                 
+     Swal.fire({
+      icon: 'success',
+      title: 'Copyed',
+      text: 'Your work has been saved on your clipboard :3',
+    })
+    };
+    function ngopilimo () {
+    document.getElementById("cmdlimo").select();
+     document.execCommand("copy");                 
+     Swal.fire({
+      icon: 'success',
+      title: 'Copyed',
+      text: 'Your work has been saved on your clipboard :3',
+    })
+    };
+    function ngopienem () {
+    document.getElementById("cmdenem").select();
+     document.execCommand("copy");                 
+     Swal.fire({
+      icon: 'success',
+      title: 'Copyed',
+      text: 'Your work has been saved on your clipboard :3',
+    })
+    };
+    function ngopipitu () {
+    document.getElementById("cmdpitu").select();
+     document.execCommand("copy");                 
+     Swal.fire({
+      icon: 'success',
+      title: 'Copyed',
+      text: 'Your work has been saved on your clipboard :3',
+    })
+    }
