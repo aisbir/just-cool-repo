1dfd8cc2e83e151046981283d128fe9533d3f881
aisbir Gans <75553444+aisbir@users.noreply.github.com>
2022-03-14 19:42:09 +0700

    Update anu.js

--- anu.js
+++ anu.js
@@ -60,4 +60,13 @@ function ngopisiji () {
       title: 'Copyed',
       text: 'Your work has been saved on your clipboard :3',
     })
+    }:
+function ngopiwolu () {
+    document.getElementById("cmdwolu").select();
+     document.execCommand("copy");                 
+     Swal.fire({
+      icon: 'success',
+      title: 'Copyed',
+      text: 'Your work has been saved on your clipboard :3',
+    })
     }
